2604636abc8bb757d896457eb0254dcb67d65f23
Thais Goulart <thaisgoulartf@gmail.com>
2021-07-21 20:28:24 -0300

    inicio do projeto Orkut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Box = styled.div`
+   background: #FFFFFF;
+   border-radius: 8px;
+   font-family: sans-serif;
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+  <Box>
+    Amigos
+  </Box>
+  )
 }
